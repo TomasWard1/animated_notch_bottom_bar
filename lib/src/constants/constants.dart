76dd055f62d87a46663a23aada9db6d70adaf9ce
Tomas Ward <35110844+TomasWard1@users.noreply.github.com>
2023-11-16 10:07:48 -0300

    Update constants.dart

--- lib/src/constants/constants.dart
+++ lib/src/constants/constants.dart
@@ -19,7 +19,7 @@ const double kTopMargin = 10.0;
 const double kBottomRadius = 0;
 
 /// bottom bar item size
-const double kIconSize = 22.0;
+const double kIconSize = 10.0;
 
 /// Pi value
 const double kPi = 3.1415926535897932;
